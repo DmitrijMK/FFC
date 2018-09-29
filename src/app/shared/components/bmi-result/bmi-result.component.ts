482b2c291e588ed141bada5a738da4e94ba5b76f
DmitrijMK <dmitrijmk@gmail.com>
2018-09-29 20:33:48 +0300

    scss

--- src/app/shared/components/bmi-result/bmi-result.component.ts
+++ src/app/shared/components/bmi-result/bmi-result.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bmi-result',
   templateUrl: './bmi-result.component.html',
-  styleUrls: ['./bmi-result.component.less']
+  styleUrls: ['./bmi-result.component.scss']
 })
 export class BmiResultComponent implements OnInit {
 
