15cf7afaf7eabe1ddf4f80751006045a4fec43f3
DmitrijMK <dmitrijmk@gmail.com>
2018-09-29 22:01:54 +0300

    fix

--- src/app/shared/services/color-theme.service.ts
+++ src/app/shared/services/color-theme.service.ts
@@ -6,10 +6,6 @@ export class ColorThemeService {
   private color$ = new BehaviorSubject('manColor');
   data$ = this.color$.asObservable();
 
-  get() {
-    return 'woman-wrapper';
-  }
-
   getNext(res) {
     console.log(res);
     this.color$.next(res);
