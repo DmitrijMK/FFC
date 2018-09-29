64d008b456cb1adae39157e1d7a9bf7dd60b437e
Vova Mahdych <xdrt@ukr.net>
2018-09-29 18:32:43 +0300

    oop

---
+++ src/app/shared/components/bmi-result/bmi-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bmi-result',
+  templateUrl: './bmi-result.component.html',
+  styleUrls: ['./bmi-result.component.less']
+})
+export class BmiResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
